36e992479e5bbd9723778c0d1e3c62a5dd48a14d
sae1013 <sae1013@gmail.com>
2022-09-25 17:43:21 +0900

    Infinite Query...

--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import {useInfiniteQuery} from "react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -9,5 +10,27 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {data,fetchNextPage,hasNextPage,isFetching,isError,error} = useInfiniteQuery(
+    'sw-people',({pageParam = initialUrl}) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined  // 스타워즈 api 에는 응답데이터에 next에 다음 url 을 담아서 보내줌. //data/pages[-1] // 응답 그자체를 매개변수로 받는다.
+    }
+  );
+
+
+
+  return(
+    <InfiniteScroll loadMore = {fetchNextPage} hasMore = {hasNextPage}>
+      {isFetching && <div className={"loading"}>Loading</div>}
+      {data?.pages?.map(pageData => {
+        return pageData.results.map(person => {
+          return <Person key={person.name} name={person.name} hairColor={person.hair_color}></Person>
+        })
+      })}
+    </InfiniteScroll>
+  );
 }
+
+// pageParam
+// 초기에는 defaultURLfh fetch를 땡기고, 다음 페이지를 fetch 할때는, getNextPageParam 에서 리턴한 값을 사용함
+// 다이어그램. 다음 페이지를 fetch 할때, getNextPageParam 에서 리턴한 값을 사용함.
--- src/species/InfiniteSpecies.jsx
+++ src/species/InfiniteSpecies.jsx
@@ -1,13 +1,32 @@
 import InfiniteScroll from "react-infinite-scroller";
+import {useInfiniteQuery} from "react-query";
+
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
 const fetchUrl = async (url) => {
+  // console.log('fetch')
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {data,fetchNextPage,hasNextPage,isFetchingNextPage,isFetching} = useInfiniteQuery('species',({pageParam = initialUrl})=>fetchUrl(pageParam),{
+    // TODO: Add Query Option
+    getNextPageParam: (lastPage,allPages) =>{
+      // console.log('getNextPAram')
+      return lastPage.next
+    }
+
+  })
+
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore = {hasNextPage}>
+      {data?.pages?.map((pageData) => {
+        return pageData.results.map((specie) => {
+          return <Species key={specie.name} {...specie}></Species>
+        })
+      })}
+    </InfiniteScroll>);
 }
+
